cc23e20441e09898bf04bb454110b634c055405c
Rainbow-Blood <dalemegan222222@gmail.com>
2025-11-07 12:09:00 +0700

    Add encode - decode utf-8

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 using PRM392_Server.Hubs;
 
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication();
+var configuration = builder.Configuration;
 
-builder.Services.AddAuthorization();
-
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Encoder =
+                System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+        }
+    );
 
 builder.Services.AddSignalR();
 
@@ -14,18 +21,10 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.UseAuthentication();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.MapHub<ChatHub>("/chatHub");
 
-app.MapGet("/",() =>
-{
-    Console.WriteLine("CONNECTED");
-    return "CONNECTED";
-});
+app.MapGet("/",() => {Console.WriteLine("CONNECTED");return "CONNECTED";});
 
 app.Run();
